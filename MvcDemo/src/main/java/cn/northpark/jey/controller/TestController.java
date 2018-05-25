a973980282a8ef9744687635810af99bbdad3319
六猴儿 <654714226@qq.com>
2018-05-25 16:36:56 +0800

    轻量级的MVC框架

--- MvcDemo/src/main/java/cn/northpark/jey/controller/TestController.java
+++ MvcDemo/src/main/java/cn/northpark/jey/controller/TestController.java
@@ -41,69 +41,19 @@ public class TestController extends BaseAction{
 	  @MapPath("/getJson")
 	  public Renderer getJson() {
 		  String name = JeyyContext().getRequest().getParameter("name");
-//	    return "<h1>Hello, " + name  +"</h1>";
 		  Map<String, Object> map = new HashMap<>();
 		  map.put("name", name);
 		  return new JsonRenderer(JsonUtil.object2json(map));
 	  }
 
 //	  @MapPath("/")
-//	  public Renderer index() throws Exception {
-//	    List posts = DbUtils.queryForList("select id, title, content, creation from Post order by id desc", new Object[0]);
-//	    return new TemplateRenderer("/index.htm", "posts", posts);
-//	  }
-//
-//	  @MapPath("/blog/display/$1")
-//	  public Renderer display(long id) throws Exception {
-//	    List posts = DbUtils.queryForList("select id, title, content, creation from Post where id=?", new Object[] { Long.valueOf(id) });
-//	    if (posts.isEmpty())
-//	      throw new IllegalArgumentException("Post not found with id: " + id);
-//	    return new TemplateRenderer("/display.htm", "post", posts.get(0));
-//	  }
-
-//	  @MapPath("/blog/create")
-//	  public Renderer create()
-//	  {
-//	    Post post = new Post(-1L, "", "");
-//	    return new TemplateRenderer("/edit.htm", "post", post);
-//	  }
-//
-//	  @MapPath("/blog/update/$1")
-//	  public Renderer update(long id)
-//	    throws Exception
-//	  {
-//	    List posts = DbUtils.queryForList("select id, title, content, creation from Post where id=?", new Object[] { Long.valueOf(id) });
-//	    if (posts.isEmpty())
-//	      throw new IllegalArgumentException("Post not found with id: " + id);
-//	    return new TemplateRenderer("/edit.htm", "post", posts.get(0));
-//	  }
-//
-//	  @MapPath("/blog/edit")
-//	  public String edit()
-//	    throws Exception
-//	  {
-//	    HttpServletRequest request = ActionContext.getActionContext().getHttpServletRequest();
-//	    long id = Long.parseLong(request.getParameter("id"));
-//	    String title = request.getParameter("title");
-//	    String content = request.getParameter("content");
-//	    if (id == -1L)
-//	    {
-//	      id = DbUtils.nextId();
-//	      if (1 != DbUtils.executeUpdate("insert into Post (id, title, content, creation) values (?, ?, ?, ?)", new Object[] { Long.valueOf(id), title, content, new Date() })) {
-//	        throw new SQLException("Create post failed.");
-//	      }
-//
-//	    }
-//	    else if (1 != DbUtils.executeUpdate("update Post set title=?, content=? where id=?", new Object[] { title, content, Long.valueOf(id) })) {
-//	      throw new IllegalArgumentException("Post not found with id: " + id);
-//	    }
-//	    return "redirect:/blog/display/" + id;
-//	  }
-//
-//	  @MapPath("/blog/delete/$1")
-//	  public String delete(long id) throws Exception {
-//	    DbUtils.executeUpdate("delete from Post where id=?", new Object[] { Long.valueOf(id) });
+//	  public Renderer returnDemo() throws Exception {
+//	    return new JsonRenderer(jsonstr);
+//	    return new TemplateRenderer("/index.html", "list", list);
+//          return new TemplateRenderer("/show.html", "entity", list.get(0));
+//	    return new OtherExtendsRenderer(otherstr,Encoding);	
+//	    return "redirect:/goods/show/" + id;
 //	    return "redirect:/";
 //	  }
+//
 
-}
